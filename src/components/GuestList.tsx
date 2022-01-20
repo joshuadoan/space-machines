27001bef260b7754d0ec7adaf83a119d14692680
pepper <insaddingaround@gmail.com>
2022-01-20 15:00:30 -0800

    Layout

--- src/components/GuestList.tsx
+++ src/components/GuestList.tsx
@@ -8,7 +8,7 @@ export default function ({ ships }: { ships: Ship[] }) {
   return (
     <ul>
       {ships.map((ship, i) => (
-        <li key={i}  >
+        <li key={i} className="flex gap-1 flex-wrap flex-col">
           <section className="flex gap-2 flex-wrap items-center">
             <span
               style={{
@@ -16,11 +16,11 @@ export default function ({ ships }: { ships: Ship[] }) {
               }}
             >{`●`}</span>
             <Link to={`/?ship=${ship.id}`}>{ship.name}</Link>
-            <Tag >{ship.state.value.type}</Tag>
           </section>
           <section className="flex gap-2 flex-wrap items-center">
             {ship.visited.length >= Total.TradeRouteDelta ? "●" : ship.visited.length > Total.TradeRouteDelta / 2 ? "◐" : "◌"}
             <span>⚡{Math.round((100 * ship.fuel || 1) / Total.Fuel)}%</span>
+            <Tag >{ship.state.value.type}</Tag>
           </section>
         </li>
       ))}
